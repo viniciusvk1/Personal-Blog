d6079f35eedc3dba71c5551700d86743c977b55d
Vinicius <viniciusalmeida.vk1@gmail.com>
2023-06-27 20:26:08 -0300

    add redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Grid } from "@material-ui/core"
+import Navbar from './components/static/navBar/Navbar';
+import Footer from './components/static/footer/Footer';
+import Home from './pages/home/Home';
 import './App.css';
-import Home from './paginas/home/Home';
-import Navbar from './components/estaticos/navbar/Navbar';
-import Footer from './components/estaticos/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
+import CadastroUsuario from './pages/cadastroUsuario/CadastroUsuario';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+import ListaTema from './components/tema/listatema/ListaTema';
+import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './components/tema/cadastroTema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './components/tema/deletarTema/DeletarTema';
+import store from './store/store';
+import { Provider } from 'react-redux';
+
+
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
+    <BrowserRouter>
       <Navbar />
-      <Home />
+      <div style={{ minHeight: '65vh' }}>
+        <Routes> // Antigo Switch
+          <Route path="/home" element={<Home />} />
+
+          <Route path="/" element={<Login />} />
+
+          <Route path="/login" element={<Login />} />
+
+          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+          
+          <Route path="/temas" element={<ListaTema />} />
+
+          <Route path="/postagem" element={<ListaPostagem />} />
+
+          <Route path="/formularioPostagem" element={<CadastroPost />} />
+
+          <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+
+          <Route path="/formularioTema" element={<CadastroTema />} />
+
+          <Route path="/formularioTema/:id" element={<CadastroTema />} />
+
+          <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+
+          <Route path="/deletarTema/:id" element={<DeletarTema />} />
+
+        </Routes>
+      </div>
       <Footer />
-    </>
+    </BrowserRouter>
+    </Provider>
   );
 }
 
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App.tsx'
+import './index.css'
+import Home from './pages/home/Home.tsx'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root') as HTMLElement
+)
